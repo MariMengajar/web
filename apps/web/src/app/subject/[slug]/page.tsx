722fdc7137827ac9e33843c7e7e10a37d71276e2
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2023-03-31 14:26:10 +0200

    💩 subject page

--- apps/web/src/app/subject/[slug]/page.tsx
+++ apps/web/src/app/subject/[slug]/page.tsx
@@ -1,23 +1,32 @@
-import useFetchSubject from '~/lib/firestore/use-fetch-subject';
+import { db, collection, getDocs, query, where, SUBJECTS_COLLECTION } from '@mentora/firebase';
+import type { Subject } from '@mentora/firebase';
 
-export default function Subject({ params }: { params: { slug: string } }) {
-  const { data: subject, status } = useFetchSubject(params.slug);
+async function getSubject(subjectSlug: string) {
+  const q = query(collection(db, SUBJECTS_COLLECTION), where('slug', '==', subjectSlug));
+  const querySnapshot = await getDocs(q);
 
-  /* data is loading */
-  if (status === `loading`) {
-    return <div>Loading...</div>;
+  if (querySnapshot.size > 1) {
+    throw new Error('There are more than one subject with the same slug');
+  } else if (querySnapshot.size === 0) {
+    throw new Error('There are no subjects with this slug');
   }
 
-  /* request errored */
-  if (status === `error`) {
-    return <div>Error!</div>;
+  const subject = querySnapshot.docs[0].data() as Subject;
+
+  return subject;
+}
+
+export default async function Subject({ params }: { params: { slug: string } }) {
+  const subject = await getSubject(params.slug);
+
+  /* data is loading */
+  if (status === 'loading') {
+    return <div>Loading...</div>;
   }
 
   return (
     <>
-      <h1>{subject.title}</h1>
-      <h2>{subject.scheduledDate.getDate()}</h2>
-      <p>{subject.description}</p>
+      <h1>Hello</h1>
     </>
   );
 }
